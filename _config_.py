5f6ce369820ee90c6e9f4a6a19456e341f032f35
Flusskrebs <flusskrebs49@gmail.com>
2026-04-09 23:19:12 +0200

    Create _config_.py

---
+++ _config_.py
@@ -0,0 +1,18 @@
+#coding: utf-8
+
+'''
+Fichier pour l'adressage des différents équipement
+
+'''
+_MQTT_ip='192.168.1.3'                          #Adresse IP du broker mqtt
+_MQTT_port=1883                                         #Port du broker mqtt
+_MQTT_authentication=True          #Mqtt use authentication
+_MQTT_user='mqttuser'                               #Mqtt User name
+_MQTT_pass='mqttuser'                               #Mqtt password
+_MQTT_TOPIC_SUB='homeassistant/SUBmcz'                  #Topic général de souscription
+_MQTT_TOPIC_PUB='homeassistant/PUBmcz'                  #Topic général de publication
+_MCZip='192.168.120.1'                          #Adresse IP du poêle
+_MCZport='81'                                           #Port du serveur embarqué du poele
+_VERSION='1.5'                                          #Version
+_AUTHOR='Anthony L.'                            #Auteur
+
